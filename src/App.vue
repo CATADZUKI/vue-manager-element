46dc8f7baba8ef8ebe3c64a2e80473d3bc666cd0
cccax <623307436@qq.com>
2021-01-07 15:57:02 +0800

    login页面

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <login></login>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Login from './views/Login.vue'
+export default {
+  name: 'app',
+  components: {
+    Login
   }
+}
+</script>
+
+<style>
+html{
+  height: 100%;
+}
+body{
+  height: 100%;
+  margin: 0px;
+}
+#app{
+  height: 100%;
+  width: 100%;
 }
 </style>
